39d1a83eeace59d2676138a48ae2f80fc0b84c21
fedchishina.victoria@gmail.com <fedchishina.victoria@gmail.com>
2017-07-14 17:19:49 +0300

    fixed bugs (tree of employers) installed faker package

--- resources/views/pages/employers/tree/content.blade.php
+++ resources/views/pages/employers/tree/content.blade.php
@@ -5,7 +5,7 @@
             @foreach ($employers as $employer)
                 <li>
                     <span>Full name:{{ $employer->full_name }}; position: {{$employer->position->name}}</span>
-                    @if( isset( $employer->children ) && count($employer->children ) > 0 )
+                    @if( isset( $employer->children ) && count($employer->children ) > 0 && ($employer->id <> $employer->parent_id))
                         @include('pages.employers.tree.children', ['employers' => $employer->children, 'test' => 'true'])
                     @endif
                 </li>
